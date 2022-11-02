47ba861fad2c9dbb6324439a7e7998e6501379cc
Daniel Vu <dvv297@gmail.com>
2022-11-02 17:10:05 -0500

    Update about page

--- src/components/ExperienceCard/ExperienceCard.jsx
+++ src/components/ExperienceCard/ExperienceCard.jsx
@@ -1,13 +1,11 @@
 import React from "react"
 
-import "./ExperienceCard.css"
-
 const ExperienceCard = ({ company, jobTitle, timeWorked, children }) => {
   return (
-    <div className="experience-card">
-      <h1>{company}</h1>
-      <div className="experience-card-subheader">
-        <small>{jobTitle}</small>
+    <div className="mb-8">
+      <h1 className="mb-2 text-2xl font-bold">{company}</h1>
+      <div className="mb-2 flex w-full flex-col text-xl md:mb-1 md:flex-row md:text-2xl">
+        <small className="flex-1">{jobTitle}</small>
         <small>{timeWorked}</small>
       </div>
       {children}
